9f074b3074f34dff55ca24b7b6637857c7d40727
MarlonJM <marlonjjm0816@gmail.com>
2016-08-09 15:22:52 -0600

    Rediseño Portafolio

--- json/jsonPersonas.php
+++ json/jsonPersonas.php
@@ -1,10 +1,6 @@
 <?php
 include '../phpclass/conectar.php';
-<<<<<<< HEAD
 include '../phpclass/persona.php';
-=======
-include '../phpclass/producto.php';
->>>>>>> origin/master
 
 $con = new Persona();
 $personas = $con->getPersonas();
