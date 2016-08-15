98bb1b13c43f0734b68b6236773c5fc1f9e792f4
MarlonJM <marlonjjm0816@gmail.com>
2016-08-14 18:42:20 -0600

    Ajax portafolio
    se añade código para poder obtener id en el javascript y enviarlo por
    parametro a la url del json por get

--- js/consultarJsonPersonaId.js
+++ js/consultarJsonPersonaId.js
@@ -3,15 +3,18 @@ var app = {
         this.cargarPersonaId();
     },
     cargarPersonaId: function(){
+        var id = getGET();
+        var urlJson = "./json/jsonPersonaId.php?id=";
+        var direc = urlJson.concat(id.id);
         $.ajax({
-            url: "./json/jsonPersonaId.php",
+            url: direc,
             type: "GET",
             dataType: "json",
             success: function(resultado){
                 $('#perfil').append(
                     '<div id="profileImg" style="background-image: url(img/'+resultado['profileImg']+')">'
                     +'</div>'
-                    +'<h1 style="padding-top:20px;">'+resultado['nombre']+' '+resultado['apellido1']
+                    +'<h1 style="padding-top:20px;">'+resultado['nombre']+' '+resultado['primerApellido']
                     +'</h1>'
                 );
             },
@@ -27,3 +30,16 @@ $(document).ready(
         app.init();
     }
 );
+
+function getGET(){
+   var loc = document.location.href;
+   var getString = loc.split('?')[1];
+   var GET = getString.split('&');
+   var get = {};
+
+   for(var i = 0, l = GET.length; i < l; i++){
+      var tmp = GET[i].split('=');
+      get[tmp[0]] = unescape(decodeURI(tmp[1]));
+   }
+   return get;
+}
