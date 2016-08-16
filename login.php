c51ee562f3aec2a782618afda5af0f7925791b6a
kat28 <kategodinezd@gmail.com>
2016-08-16 13:26:55 -0600

    logo login

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" type="text/css" href="css/styleLogin.css">
 <meta charset="UTF-8">
 
+
 <div class="login">
 
 	<?php if(isset($_GET['error']) && $_GET['error'] == 'acceso-denegado') { ?>
@@ -10,8 +11,8 @@
 	<?php } ?>
 
 	<form action="validarLogin.php" method="post">
-		<div id="title">
-			<p>PKMS Design</p>
+		<div id="logo">
+			<img src="img/LOGO1_opt.png">
 		</div>
 		<input type="text" name="user" class="login-input" placeholder="Usuario" autofocus>
 		<input type="password" name="password" class="login-input" placeholder="Contraseña">
@@ -23,4 +24,5 @@
 			<p>Usuario o contraseña incorrecta</p>
 		</div>
 	<?php } ?>
+
 </div>
